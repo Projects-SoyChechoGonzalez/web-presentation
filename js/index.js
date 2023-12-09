492abe775ce114782ee0d9f97d226bab9b8f62d1
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2023-12-08 22:39:07 -0500

    feat: se sube proyecto

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+	menu.classList.toggle('mostrar');
+});
+
+const close = document.querySelector('nav');
+close.addEventListener('click', () => {
+	menu.classList.remove('mostrar');
+});
